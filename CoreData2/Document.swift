b2bccced78404eb04c92092c00a3a512d4ca69f9
Sarah-Alqahtani <sara123.it20@gmail.com>
2021-12-28 22:37:12 +0000

    Update

--- CoreData2/Document.swift
+++ CoreData2/Document.swift
@@ -5,5 +5,10 @@
 //  Created by admin on 28/12/2021.
 //
 
+// What do you think would trigger the  catch statement below?
+      
+       //1-forget to import core data and its happened a lot specially for the people training or studying Swift language
+      //2- we should load any persistent stores
+      //3- We should create a variable to hold a reference to a persistent container in ViewController or subclass
 
-
+ 
